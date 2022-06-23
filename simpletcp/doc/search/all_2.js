11b195b519e971a681e9e8a883d9ef00f79919a1
dev-cmurphy <charles.murphy.042@gmail.com>
2022-06-23 19:46:32 +0000

    Update from https://github.com/dev-cmurphy/simpletcp/commit/338045a4fdbc2d865e19253540e11cf7e1f3d099

--- simpletcp/doc/search/all_2.js
+++ simpletcp/doc/search/all_2.js
@@ -3,5 +3,5 @@ var searchData=
   ['sendtoallclients_7',['SendToAllClients',['../classcmurphy_1_1simpletcp_1_1server_1_1_t_c_p_server.html#ae56f85c69f25458bc277af943ddcb6fb',1,'cmurphy::simpletcp::server::TCPServer']]],
   ['serverparameters_8',['ServerParameters',['../classcmurphy_1_1simpletcp_1_1server_1_1_server_parameters.html',1,'cmurphy::simpletcp::server']]],
   ['servershutdown_9',['ServerShutdown',['../classcmurphy_1_1simpletcp_1_1server_1_1_t_c_p_server.html#a6b6cb69ec8dfa5f605612a80be9f1b74',1,'cmurphy::simpletcp::server::TCPServer']]],
-  ['serverstart_10',['ServerStart',['../classcmurphy_1_1simpletcp_1_1server_1_1_t_c_p_server.html#ab981a826509a9fc84869617e6e4635c6',1,'cmurphy::simpletcp::server::TCPServer']]]
+  ['serverstart_10',['ServerStart',['../classcmurphy_1_1simpletcp_1_1server_1_1_t_c_p_server.html#ac55c1441f5afdce4235a9c2dd0f1ae7f',1,'cmurphy.simpletcp.server.TCPServer.ServerStart()'],['../classcmurphy_1_1simpletcp_1_1server_1_1_t_c_p_server.html#ab981a826509a9fc84869617e6e4635c6',1,'cmurphy.simpletcp.server.TCPServer.ServerStart(ServerParameters parameters)']]]
 ];
